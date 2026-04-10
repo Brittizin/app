8f5bd25cb7c3c5ea30eafceb4daeed3ef4d62bc3
ILuizkk <luizgabrielhsg@gmail.com>
2026-04-10 18:57:22 -0300

    Update Android remote app to landscape fullscreen build

--- android-app/app/src/main/java/com/pocketdeck/remote/MainActivity.java
+++ android-app/app/src/main/java/com/pocketdeck/remote/MainActivity.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
@@ -34,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private Button retryButton;
     private Button configButton;
     private Button topConfigButton;
-    private Button pageButton;
+    private Button prevPageButton;
+    private Button nextPageButton;
     private TextView titleText;
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -42,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         webView = findViewById(R.id.webView);
         errorCard = findViewById(R.id.errorCard);
@@ -49,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
         retryButton = findViewById(R.id.retryButton);
         configButton = findViewById(R.id.configButton);
         topConfigButton = findViewById(R.id.topConfigButton);
-        pageButton = findViewById(R.id.pageButton);
+        prevPageButton = findViewById(R.id.prevPageButton);
+        nextPageButton = findViewById(R.id.nextPageButton);
         titleText = findViewById(R.id.titleText);
 
         titleText.setText(getString(R.string.app_name));
@@ -71,7 +75,8 @@ public class MainActivity extends AppCompatActivity {
         retryButton.setOnClickListener(v -> loadRemote());
         configButton.setOnClickListener(v -> openConfigDialog());
         topConfigButton.setOnClickListener(v -> openConfigDialog());
-        pageButton.setOnClickListener(v -> triggerPageChange());
+        prevPageButton.setOnClickListener(v -> triggerPageChange("remotePrevPageBtn"));
+        nextPageButton.setOnClickListener(v -> triggerPageChange("remoteNextPageBtn"));
 
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
@@ -87,9 +92,35 @@ public class MainActivity extends AppCompatActivity {
         loadRemote();
     }
 
-    private void triggerPageChange() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        applyImmersiveMode();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            applyImmersiveMode();
+        }
+    }
+
+    private void applyImmersiveMode() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+    }
+
+    private void triggerPageChange(String buttonId) {
         webView.evaluateJavascript(
-            "(() => { const btn = document.querySelector('#remotePageBtn'); if (btn) { btn.click(); return true; } return false; })();",
+            "(() => { const btn = document.querySelector('#" + buttonId + "'); if (btn) { btn.click(); return true; } return false; })();",
             null
         );
     }
